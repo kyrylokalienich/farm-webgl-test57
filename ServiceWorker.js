9762c5f7a496c41a5a2ef3bd71f31dc2e42cdbdc
kyrylokalienich <155952445+kyrylokalienich@users.noreply.github.com>
2024-06-10 16:48:02 +0300

    Update build. Stable (Relay + Lobby)

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,9 +1,9 @@
-const cacheName = "DefaultCompany-Test-Tranport-Mathcmaking-0.1.0";
+const cacheName = "DefaultCompany-ChessTG-1.0";
 const contentToCache = [
-    "Build/b1359e8348c605a463964efa08bf3880.loader.js",
-    "Build/8633bea1de86e22b8f3cfded512a00a9.framework.js",
-    "Build/298513a5c8ba3111a7686607eb575bad.data",
-    "Build/f49a3df8edb32f5529c558e8fd7c9b5f.wasm",
+    "Build/c77f8d9f349436a6d47d357565ed9b6d.loader.js",
+    "Build/981ec1e008b9e4ee6663ddbf84f85da4.framework.js",
+    "Build/6c82fdfde829f0bca33c2024e7b9d035.data",
+    "Build/da360ac30674dc4e890510f0fc30afc5.wasm",
     "TemplateData/style.css"
 
 ];
